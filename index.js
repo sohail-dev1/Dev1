63f15fe05e90b64799c6fbf03b2b06cb152596ee
sohail-dev1 <sohailabbas392001@gmail.com>
2024-10-17 22:11:46 +0500

    fifth commit

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ console.log("hello developers")
 console.log("my name is sohail abbas ")
 console.log("i am here to complete my task")
 console.log("to change author name in commit")
-console.log("first i want to create another github account ")
+
+console.log("we have some commands")
